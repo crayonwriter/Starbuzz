2f49bf99d21a521b1eb4299a61d247ff76eff0cb
Theda Rogers <crayonwriter@gmail.com>
2019-10-14 23:22:45 -0400

    Update: Add code to FoodCategoryActivity class

--- app/src/main/java/com/hfad/starbuzz/FoodCategoryActivity.java
+++ app/src/main/java/com/hfad/starbuzz/FoodCategoryActivity.java
@@ -1,7 +1,15 @@
 package com.hfad.starbuzz;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class FoodCategoryActivity extends Activity {
+    public static final String EXTRA_FOODID = "foodId";
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food);
+    }
 }
