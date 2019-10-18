3580453920e842ce3d078fd715b9c5c99daa43b0
Theda Rogers <crayonwriter@gmail.com>
2019-10-17 23:30:09 -0400

    Update: Filled in code for java of foodcategoryactivity

--- app/src/main/java/com/hfad/starbuzz/FoodCategoryActivity.java
+++ app/src/main/java/com/hfad/starbuzz/FoodCategoryActivity.java
@@ -1,13 +1,67 @@
 package com.hfad.starbuzz;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.view.View;
+import android.content.Intent;
+import android.widget.AdapterView;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
-public class FoodCategoryActivity extends AppCompatActivity {
+public class FoodCategoryActivity extends Activity {
+
+    private SQLiteDatabase db;
+    private Cursor cursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_category);
+        ListView listFoods = (ListView) findViewById(R.id.list_foods);
+        SQLiteOpenHelper starbuzzDatabaseHelper = new StarbuzzDatabaseHelper(this);
+        try {
+            db = starbuzzDatabaseHelper.getReadableDatabase();
+            cursor = db.query("FOOD",
+                    new String[] {"_id", "NAME"},
+                    null, null, null, null, null);
+
+            SimpleCursorAdapter listAdapter = new SimpleCursorAdapter(this,
+                    android.R.layout.simple_list_item_1,
+                    cursor,
+                    new String[] {"NAME"},
+                    new int[] {android.R.id.text1},
+                    0);
+            listFoods.setAdapter(listAdapter);
+        } catch (SQLiteException e) {
+            Toast toast = Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
+
+
+        //Create the listener
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> listFoods, View itemView, int position, long id) {
+                //Pass the food the user clicks on to FoodActvity
+                Intent intent = new Intent(FoodCategoryActivity.this, FoodActivity.class);
+                intent.putExtra(FoodActivity.EXTRA_FOODID, (int) id);
+                startActivity(intent);
+            }
+        };
+
+        //Assign the listener to the list view
+        listFoods.setOnItemClickListener(itemClickListener);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        cursor.close();
+        db.close();
     }
 }
