61eb93d59abaaf3d0eee4f1f2e1536b145a3e5a3
Theda Rogers <crayonwriter@gmail.com>
2019-10-17 22:57:06 -0400

    Update: Create FoodCategoryActivity java and layout files

--- app/src/main/java/com/hfad/starbuzz/FoodCategoryActivity.java
+++ app/src/main/java/com/hfad/starbuzz/FoodCategoryActivity.java
@@ -3,18 +3,11 @@ package com.hfad.starbuzz;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class FoodActivity extends AppCompatActivity {
-
-    public static final String EXTRA_FOODID = "foodId";
+public class FoodCategoryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_food);
-
-        //Get the food from the intent
-
+        setContentView(R.layout.activity_food_category);
     }
-
-
 }
